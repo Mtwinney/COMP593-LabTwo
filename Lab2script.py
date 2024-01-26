9f4fe86c4b17fa564988568b7222be88fd09d5bd
Mtwinney <mtwinney@flemingcollege.ca>
2024-01-26 12:43:56 -0500

    Changed name of script / Finished code
    I wanted to change the name of the file so I copy and pasted my previous code into a new document with a new name and deleted the old.
    
    I made 2 different scripts because one of them runs when you just straight off press the arrow button but the other should run straight off "python lab2script.py"
    
    As well as the final product.

---
+++ Lab2script.py
@@ -0,0 +1,79 @@
+import os
+import sys
+def main():
+    # TODO: Step 2 - Create a complex data structure
+    about_me = {   
+        'full_name': 'Michael Twinney',
+        'student_id': 1564,
+        'pizza_toppings': ['Pepperoni', 'Cheese', 'Sausage'],
+        'movies': [
+            {'title': 'Isle Of Dogs', 'genre': 'stop-motion'},
+            {'title': 'Django', 'genre': 'Western'}
+        ]   
+    }                                      
+
+
+    # TODO: Step 3 - Add another movie to the data structure
+    extra_movie = {'title': 'The Dirt', 'genre': 'documentary-esque'}
+    about_me['movies'].append(extra_movie)
+
+# TODO: Step 4 - Function that prints student name and ID	
+def print_student_name_and_id(about_me):
+    if 'full_name' in about_me and 'student_id' in about_me:
+        first_name = about_me['full_name'].split()[0]
+
+        title = 'King'
+        print(f"My name is {about_me['full_name']}, but you can call me {title} {first_name}.")
+        print(f"My student ID is {about_me['student_id']}")
+        print()
+    return
+    
+# TODO: Step 5 - Function that adds pizza toppings to data structure
+def add_pizza_toppings(about_me, toppings):
+    if 'pizza_toppings' in about_me:
+        about_me['pizza_toppings'].extend(toppings)
+        about_me['pizza_toppings'] = sorted([toppings.lower() for topping in about_me['pizza_toppings']])
+
+        toppings = ('Bacon', 'Peppers') 
+        
+
+        return
+
+# TODO: Step 6 - Function that prints bullet list of pizza toppings
+def print_pizza_toppings(about_me):
+    if 'pizza_toppings' in about_me:
+        print("My favourite pizza toppings are:")
+        for topping in about_me['pizza toppings']:
+            print(f"- {topping}")
+        print()
+    return
+
+# TODO: Step 7 - Function that prints comma-separated list of movie genres
+def print_movie_genres(about_me):
+    if 'movies' in about_me:
+        genres = [movie['genre'] for movie in about_me['movies']]
+    if len(genres) == 1:
+        print(f"I like to watch {genres[0]} movies.")
+    else:
+        print("I like to watch", end=" ")
+        for i in range(len(genres) - 1):
+            print(f"{genres[i]}, ", end ="")
+
+        print(f"and {genres[-1]} movies.")
+    return 
+
+# TODO: Step 8 - Function that prints comma-separated list of movie titles
+def print_movie_titles(movie_list):
+    if movie_list:
+        movie_titles = [movie['title'].title() for movie in movie_list]
+        if len(movie_titles) == 1:
+            print(f"Some of my favourite movies are {movie_titles[0]}!")
+        else:
+            print("Some of my favourite movies are", end=" ")
+            for i in range(len(movie_titles) - 1):
+                print(f"{movie_titles[i].title()}, ", end="")
+                print(f"and {movie_titles[-1]}!")
+    return
+    
+if __name__ == '__main__':
+    main()
--- lab2script.1.py
+++ lab2script.1.py
@@ -1,6 +1,7 @@
+import os
+import sys
+
 def main():
-
-
     # TODO: Step 2 - Create a complex data structure
     about_me = {   
         'full_name': 'Michael Twinney',
@@ -12,51 +13,65 @@ def main():
         ]   
     }                                      
 
-
     # TODO: Step 3 - Add another movie to the data structure
     extra_movie = {'title': 'The Dirt', 'genre': 'documentary-esque'}
     about_me['movies'].append(extra_movie)
 
-# TODO: Step 4 - Function that prints student name and ID	
+    # Call the functions to print the information
+    print_student_name_and_id(about_me)
+    add_pizza_toppings(about_me, ('Bacon', 'Peppers'))
+    print_pizza_toppings(about_me)
+    print_movie_genres(about_me)
+    print_movie_titles(about_me['movies'])
+
+# TODO: Step 4 - Function that prints student name and ID    
 def print_student_name_and_id(about_me):
-    if 'full_name' in about_me and 'identification' in about_me:
+    if 'full_name' in about_me and 'student_id' in about_me:
         first_name = about_me['full_name'].split()[0]
 
         title = 'King'
         print(f"My name is {about_me['full_name']}, but you can call me {title} {first_name}.")
-        print(f"My student ID is {about_me['identification']}")
-    return
-    
+        print(f"My student ID is {about_me['student_id']}")
+        print()
+
 # TODO: Step 5 - Function that adds pizza toppings to data structure
 def add_pizza_toppings(about_me, toppings):
     if 'pizza_toppings' in about_me:
         about_me['pizza_toppings'].extend(toppings)
-
-        about_me['pizza_toppings'] = sorted([toppings.lower() for topping in about_me['pizza_toppings']])
-
-        print("Added more toppings:", about_me['pizza_toppings'])
-   
-        toppings = ('Bacon', 'Peppers') 
-        
-
-        return
+        about_me['pizza_toppings'] = sorted([topping.lower() for topping in about_me['pizza_toppings']])
 
 # TODO: Step 6 - Function that prints bullet list of pizza toppings
 def print_pizza_toppings(about_me):
     if 'pizza_toppings' in about_me:
         print("My favourite pizza toppings are:")
-        for topping in about_me['pizza toppings']:
+        for topping in about_me['pizza_toppings']:
             print(f"- {topping}")
         print()
-    return
 
 # TODO: Step 7 - Function that prints comma-separated list of movie genres
 def print_movie_genres(about_me):
-    return 
+    if 'movies' in about_me:
+        genres = [movie['genre'] for movie in about_me['movies']]
+        if len(genres) == 1:
+            print(f"I like to watch {genres[0]} movies.")
+        else:
+            print("I like to watch", end=" ")
+            for i in range(len(genres) - 1):
+                print(f"{genres[i]}, ", end ="")
+
+            print(f"and {genres[-1]} movies.")
 
 # TODO: Step 8 - Function that prints comma-separated list of movie titles
 def print_movie_titles(movie_list):
-    return
-    
+    if movie_list:
+        movie_titles = [movie['title'].title() for movie in movie_list]
+        if len(movie_titles) == 1:
+            print(f"Some of my favourite movies are {movie_titles[0]}!")
+        else:
+            print("Some of my favourite movies are", end=" ")
+            for i in range(len(movie_titles) - 1):
+                print(f"{movie_titles[i]}, ", end="")
+            print(f"and {movie_titles[-1]}!")
+
 if __name__ == '__main__':
     main()
